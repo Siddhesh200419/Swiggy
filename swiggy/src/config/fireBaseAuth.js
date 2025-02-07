50c0546928bf7e91cebc527fcccd25b20499a46f
Siddhesh <siddheshgite2004@gmail.com>
2025-02-07 22:36:34 +0530

    firebaseconfig

--- swiggy/src/config/fireBaseAuth.js
+++ swiggy/src/config/fireBaseAuth.js
@@ -7,7 +7,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth"
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = JSON.parse(import.meta.env.VITE_KEY)
+const firebaseConfig = {
+    "apiKey": import.meta.env.APIKEY ,
+    "authDomain": import.meta.env.VITE_AUTHDOMAIN ,
+    "projectId": import.meta.env.VITE_PROJECTID,
+    "storageBucket":import.meta.env.VITE_STORAGEBUCKET ,
+    "messagingSenderId":import.meta.env.VITE_MESSAGINGSENDERID ,
+    "appId":import.meta.env.VITE_APPID ,
+    "measurementId": import.meta.env.VITE_MEASUREMENTID
+}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
